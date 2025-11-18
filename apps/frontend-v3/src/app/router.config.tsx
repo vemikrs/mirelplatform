0ab14945fdf5c33ff7622c14f078ed6aa0a7aa32
Hiroki Kurosawa <s@vemi.jp>
2025-11-18 16:17:48 +0900

    feat(router): Update StencilEditor route to support wildcard path

--- apps/frontend-v3/src/app/router.config.tsx
+++ apps/frontend-v3/src/app/router.config.tsx
@@ -32,7 +32,7 @@ export const router = createBrowserRouter([
         element: <StencilListPage />,
       },
       {
-        path: 'promarker/editor/:stencilId/:serial',
+        path: 'promarker/editor/*',
         element: <StencilEditor />,
       },
       {
