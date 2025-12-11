fc7870203659ce351e84e048d6884967064e74b6
Hiroki Kurosawa <s@vemi.jp>
2025-12-11 13:08:16 +0900

    fix compile error

--- backend/src/main/java/jp/vemi/mirel/foundation/web/api/auth/controller/AuthenticationController.java
+++ backend/src/main/java/jp/vemi/mirel/foundation/web/api/auth/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import jp.vemi.mirel.foundation.web.api.dto.ApiRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
